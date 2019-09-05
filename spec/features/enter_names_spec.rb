2213abd684eb1b53c7defb95cb4985ef268be7a5
Neil Campbell <neildcampbell@hotmail.com>
2019-09-05 05:46:23 +0100

    added second test points

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,9 +3,19 @@ require "./spec/spec_helper.rb"
   feature 'Enter names' do
     scenario 'submitting names' do
       visit('/')
-      fill_in :player_1, with: 'Bob'
-      fill_in :player_2, with: 'Marley'
+      fill_in :player_1_name, with: 'Bob'
+      fill_in :player_2_name, with: 'Marley'
       click_button 'Submit'
       expect(page).to have_content 'Bob vs Marley!!'
+    end
+  end
+
+  feature 'See Hit Points' do
+    scenario 'seeing points of Player 2' do
+      visit('/')
+      fill_in :player_1_name, with: 'Bob'
+      fill_in :player_2_name, with: 'Marley'
+      click_button 'Submit'
+      expect(page). to have_content 'Marley: 60HP'
     end
   end
