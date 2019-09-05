918fdae12f69ea2e05910916bc358023799a2721
Neil Campbell <neildcampbell@hotmail.com>
2019-09-05 06:36:19 +0100

    add link to attack

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,21 +1,24 @@
 require "./spec/spec_helper.rb"
+require_relative "web_helpers"
 
   feature 'Enter names' do
     scenario 'submitting names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Bob'
-      fill_in :player_2_name, with: 'Marley'
-      click_button 'Submit'
+      sign_in_and_play
       expect(page).to have_content 'Bob vs Marley!!'
     end
   end
 
   feature 'See Hit Points' do
-    scenario 'seeing points of Player 2' do
-      visit('/')
-      fill_in :player_1_name, with: 'Bob'
-      fill_in :player_2_name, with: 'Marley'
-      click_button 'Submit'
+    scenario 'seeing points of Player 2'do
+      sign_in_and_play
       expect(page). to have_content 'Marley: 60HP'
+    end
+  end
+
+  feature 'Attack' do
+    scenario 'Player 1 attacks Player 2 and gets confirmation'do
+      sign_in_and_play
+      click_link 'Attack!'
+      expect(page). to have_content 'Bob attacks Marley!'
     end
   end
---
+++ views/attack.rb
@@ -0,0 +1,1 @@
+<h1> <%= @player_1_name %> attacks <%= @player_2_name %> ! </h1>
